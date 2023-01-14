86421423e097603afa7e6c8050c28c27767202e3
RR <rajko.fed@gmail.com>
2023-01-14 19:15:21 +0100

    cors

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-require('dotenv/config')
+const cors = require('cors');
+require('dotenv/config');
 
+app.use(cors())
 app.use(bodyParser.json());
 
 const citiesRoute = require('./routes/cities');
